8876ca112c0f9c3a9c9e672b684040e1576810c2
Guillermo Martin Gastelum Rodriguez <guillegasr@MacBook-Pro.local>
2021-11-23 15:23:41 -0600

    final fixes

--- util/Raspberry/on_event.py
+++ util/Raspberry/on_event.py
@@ -7,24 +7,21 @@ account = "820212852497"
 environment = "beta"
 userId = "5636267"
 
-QueueUrl = f"https://sqs.us-east-1.amazonaws.com/{account}/{environment}-{userId}-data-consumer"
+QueueUrl = f"https://sqs.us-east-1.amazonaws.com/{account}/{environment}-{userId}-data-event"
 
 if __name__ == '__main__':
-    try:
-        while(True):
-            client_sqs = session.client('sqs')
-            response = client_sqs.receive_message(
-                QueueUrl=QueueUrl
-            )
-            if response.get("Messages",None):
-                message = response['Messages'][0]
-                receipt_handle = message['ReceiptHandle']
+    while(True):
+        client_sqs = session.client('sqs')
+        response = client_sqs.receive_message(
+            QueueUrl=QueueUrl
+        )
+        if response.get("Messages",None):
+            message = response['Messages'][0]
+            receipt_handle = message['ReceiptHandle']
 
-                client_sqs.delete_message(
-                    QueueUrl=QueueUrl,
-                    ReceiptHandle=receipt_handle
-                )
-                print('Received and deleted message: %s' % message)
-            time.sleep(5)
-    except:
-        print("Something went wrong.")
+            client_sqs.delete_message(
+                QueueUrl=QueueUrl,
+                ReceiptHandle=receipt_handle
+            )
+            print('Received and deleted message: %s' % message)
+        time.sleep(5)
